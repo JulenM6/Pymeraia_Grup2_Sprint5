44d647d1c5f728c7c9978fed054a2a8ba461777c
fran <franciscozahinos@iesmontsia.org>
2023-02-22 18:46:58 +0100

    línea sobrante

--- resources/views/report/show.blade.php
+++ resources/views/report/show.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <div class=" overflow-x-auto shadow-md sm:rounded-lg m-5">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
