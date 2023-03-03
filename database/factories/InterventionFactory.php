2fc5e48675576fe7d7a3a43e6f0838cde0731b5b
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2023-03-03 14:43:27 +0100

    factory modificado

--- database/factories/InterventionFactory.php
+++ database/factories/InterventionFactory.php
@@ -14,11 +14,14 @@ class InterventionFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    public static $counter = 0;
+
     public function definition()
     {
+        $array = ['Inmediata', 'A largo plazo', 'A corto plazo'];
         return [
-            'name' => $this->faker->unique()->name(),
-            'hidden' => $this->faker->date(),
+            'name' => $array[self::$counter++ % count($array)],
         ];
     }
 }
