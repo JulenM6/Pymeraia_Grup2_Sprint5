7e57c0b51410b36158ea91c7501b52aa7a75dee0
Fran Zahinos <franciscozahinos@iesmontsia.org>
2023-03-07 20:15:30 +0100

    Revert "retoques seeders/factories"
    This reverts commit 35ddeee8a03443039b37c443c469cc4d1e25cb31.

--- database/seeders/QuestionQuestionnaireSeeder.php
+++ database/seeders/QuestionQuestionnaireSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Question;
+use App\Models\QuestionQuestionnaire;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class QuestionSeeder extends Seeder
+class QuestionQuestionnaireSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,6 +15,6 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-        Question::factory(12)->create();
+        QuestionQuestionnaire::factory(12)->create();
     }
 }
