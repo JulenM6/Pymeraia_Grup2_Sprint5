ecb08d18bc2d3cf3dab4740624f20621a1285f49
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-03-07 22:05:11 +0100

    pdf funcionant

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Report;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
 use PDF;
 
 class ReportController extends Controller
@@ -36,10 +35,32 @@ class ReportController extends Controller
         return view('report.show', compact('report'));
     }
 
-    public function pdf()
+    public function pdf($id)
     {
-        $report = Report::all();
-        $pdf = PDF::loadView('report.pdf', compact('report'));
-        return $pdf->download('report.pdf');
+
+        $report = DB::table('reports')
+            ->select('answers.id', 'answers.name as answers', 'answers.recommendation as recommendation', 'questions.name as questions', 'type_measures.name as type_measures', 'risks.name as risks', 'probabilities.name as probabilities', 'interventions.name as interventions', 'impacts.name as impacts')
+            ->join('answer_report', 'answer_report.report_id', '=', 'reports.id')
+            ->join('answers', 'answer_report.answer_id', '=', 'answers.id')
+            ->join('impacts', 'impacts.id', '=', 'answers.impact_id')
+            ->join('interventions', 'interventions.id', '=', 'answers.intervention_id')
+            ->join('probabilities', 'probabilities.id', '=', 'answers.probability_id')
+            ->join('questions', 'questions.id', '=', 'answers.question_id')
+            ->join('risks', 'risks.id', '=', 'answers.risk_id')
+            ->join('type_measures', 'type_measures.id', '=', 'answers.type_measure_id')
+            ->where('reports.id', '=', $id)
+            ->get();
+
+        $pdf = PDF::loadView('report.pdf', compact('report'))->setPaper('legal', 'landscape');
+        return $pdf->stream();
+        // return $pdf->download('Informe numero: ' . $id . '.pdf');
     }
+
+
+    // public function pdf()
+    // {
+    //     $report = Report::all();
+    //     $pdf = PDF::loadView('report.pdf', compact('report'));
+    //     return $pdf->download('report.pdf');
+    // }
 }
--- resources/views/report/pdf.blade.php
+++ resources/views/report/pdf.blade.php
@@ -1,75 +1,44 @@
-@extends('layouts.app')
-
-@section('content')
-    <div>
-        <table>
-            <thead>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Informe </title>
+</head>
+<body>
+    <h1 class="tit-pdf-report"></h1>
+    <div class=" overflow-x-auto shadow-md sm:rounded-lg m-5">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('table.id') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.answer') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.recommendation') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.question') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.type.measure') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.risk') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.probability') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.intervention') }}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('report.impact') }}
-                    </th>
+                    <th scope="col" class="px-6 py-3">{{ __('table.id') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.answer') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.recommendation') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.question') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.type.measure') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.risk') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.probability') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.intervention') }}</th>
+                    <th scope="col" class="px-6 py-3">{{ __('report.impact') }}</th>
                 </tr>
             </thead>
             <tbody>
-
                 @foreach ($report as $row)
                     <tr>
-
-                        <th scope="row">
-                            {{ $row->id }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $row->answers }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->recommendation }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->questions }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->type_measures }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->risks }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->probabilities }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->interventions }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $row->impacts }}
-                        </td>
+                        <th scope="row">{{ $row->id }}</th>
+                        <td class="px-6 py-4">{{ $row->answers }}</td>
+                        <td class="px-6 py-4">{{ $row->recommendation }}</td>
+                        <td class="px-6 py-4">{{ $row->questions }}</td>
+                        <td class="px-6 py-4">{{ $row->type_measures }}</td>
+                        <td class="px-6 py-4">{{ $row->risks }}</td>
+                        <td class="px-6 py-4">{{ $row->probabilities }}</td>
+                        <td class="px-6 py-4">{{ $row->interventions }}</td>
+                        <td class="px-6 py-4">{{ $row->impacts }}</td>
                     </tr>
                 @endforeach
-
             </tbody>
         </table>
     </div>
-@endsection
+</body>
+</html>
