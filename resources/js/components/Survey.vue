39202f4ea7a3c5caf1749035e529fbf3ee2a0e4b
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2023-03-20 20:07:24 +0100

    fix survey

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -5,105 +5,96 @@
             <div class="px-4 py-5 sm:px-6 bg-orange-400 text-white">
                 <h3 class="text-base font-semibold leading-6">{{ $t('survey.name') }} {{ survey.name }}</h3>
             </div>
-            <form @submit.prevent="submitAnswers">
-                <div class="border-t border-gray-200">
-                    <dl v-for="question in paginatedQuestions" :key="question.id">
-                        <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-medium text-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                    class="inline-block w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                </svg>
-                                {{ $t('survey.question') }}</dt>
-                            <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">{{ question.description }}</dd>
-                        </div>
-                        <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-medium text-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                    class="inline-block w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
-                                </svg>
-                                {{ $t('survey.answer') }}</dt>
-                            <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">
-                                <div v-for="(answer, index) in question.answers" :key="answer.id"
-                                    class="flex items-center mb-4">
-                                    <input :id="'respuesta-' + index" type="radio" :name="'answer-group-' + question.id"
-                                        :value="answer.id"
-                                        class="w-4 h-4 border-gray-300 focus:ring-2 text-orange-600 focus:ring-orange-300 dark:focus:ring-orange-600 dark:focus:bg-orange-600 dark:bg-gray-700 dark:border-gray-600"
-                                        @change="selectAnswer(answer.id)"
-                                        :checked="answers.find(a => a.currentPage === currentPage && a.answerId === answer.id) !== undefined">
-                                    <label :for="'respuesta-' + index"
-                                        class="block ml-2 font-medium text-gray-900 dark:text-gray-300">
-                                        {{ answer.name }}
-                                    </label>
-                                </div>
-                            </dd>
-                        </div>
-                        <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="font-medium text-gray-500">{{ $t('survey.percent') }}</dt>
-                            <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">{{ percentageAnswered }} %
-                            </dd>
-                        </div>
-
-                        <div class="bg-orange-50 px-4 py-5 sm:gap-4 sm:px-6">
-                            <div class="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
-                                <div class="h-6 bg-blue-600 rounded-full dark:bg-blue-500"
-                                    :style="'width: ' + percentageAnswered + '%'"></div>
+            <div class="border-t border-gray-200">
+                <dl v-for="question in paginatedQuestions" :key="question.id">
+                    <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="font-medium text-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                            </svg>
+                            {{ $t('survey.question') }}</dt>
+                        <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">{{ question.description }}</dd>
+                    </div>
+                    <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="font-medium text-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                            </svg>
+                            {{ $t('survey.answer') }}</dt>
+                        <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">
+                            <div v-for="(answer, index) in question.answers" :key="answer.id"
+                                class="flex items-center mb-4">
+                                <input :id="'respuesta-' + index" type="radio" :name="'answer-group-' + question.id"
+                                    :value="answer.id"
+                                    class="w-4 h-4 border-gray-300 focus:ring-2 text-orange-600 focus:ring-orange-300 dark:focus:ring-orange-600 dark:focus:bg-orange-600 dark:bg-gray-700 dark:border-gray-600"
+                                    @change="selectAnswer(answer.id)"
+                                    :checked="answers.find(a => a.currentPage === currentPage && a.answerId === answer.id) !== undefined">
+                                <label :for="'respuesta-' + index"
+                                    class="block ml-2 font-medium text-gray-900 dark:text-gray-300">
+                                    {{ answer.name }}
+                                </label>
                             </div>
-                        </div>
-                    </dl>
+                        </dd>
+                    </div>
+                    <div class="bg-orange-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="font-medium text-gray-500">{{ $t('survey.percent') }}</dt>
+                        <dd class="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">{{ percentageAnswered }} %
+                        </dd>
+                    </div>
 
-                    <!-- paginación -->
-                    <div class="p-3 text-gray-700 uppercase bg-orange-100 dark:text-gray-400">
-                        <div class="row">
-                            <div class="col-12">
-                                <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
-                                    <button type="text"
-                                        class="relative inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-                                        :class="{ 'opacity-50 cursor-not-allowed': currentPage === 1 }"
-                                        :disabled="currentPage === 1" @click="currentPage--">&laquo; {{
-                                            $t('pagination.previous') }}</button>
-                                    <button type="text"
-                                        class="relative inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-                                        :class="{ 'opacity-50 cursor-not-allowed': currentPage === pageCount }"
-                                        :disabled="currentPage === pageCount" @click="currentPage++">{{
-                                            $t('pagination.next') }}
-                                        &raquo;</button>
-                                    <button v-if="surveySend" type="submit"
-                                        class="relative inline-flex items-center rounded-md bg-orange-600 py-2 px-3 font-semibold text-white hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-                                        :class="{ 'opacity-50 cursor-not-allowed': hasError === 0 }"
-                                        :disabled="hasError === 0">
-                                        <PaperAirplaneIcon class="h-5 w-5 mr-1" aria-hidden="true" /> {{ $t('survey.send')
-                                        }}
-                                    </button>
-                                </nav>
-                            </div>
+                    <div class="bg-orange-50 px-4 py-5 sm:gap-4 sm:px-6">
+                        <div class="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
+                            <div class="h-6 bg-blue-600 rounded-full dark:bg-blue-500"
+                                :style="'width: ' + percentageAnswered + '%'"></div>
                         </div>
                     </div>
-                    <!-- mostrar mensaje error/success -->
-                    <div class="p-4 text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert"
-                        v-if="hasError === 0">
-                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 inline-block" xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
-                            </circle>
-                            <path class="opacity-75" fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                            </path>
-                        </svg>
-                        <span class="font-medium">{{ $t('survey.completed') }}</span> {{ $t('survey.success', {
-                            seconds:
-                                '5'
-                        }) }}
-                    </div>
-                    <div class="p-4 text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert"
-                        v-if="hasError === 1">
-                        <span class="font-medium">{{ $t('survey.error') }}</span> {{ $t('survey.failed') }}
+                </dl>
+
+                <!-- paginación -->
+                <div class="p-3 text-gray-700 uppercase bg-orange-100 dark:text-gray-400">
+                    <div class="row">
+                        <div class="col-12">
+                            <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
+                                <button
+                                    class="relative inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                                    :class="{ 'opacity-50 cursor-not-allowed': currentPage === 1 }"
+                                    :disabled="currentPage === 1" @click="currentPage--">&laquo; {{
+                                        $t('pagination.previous') }}</button>
+                                <button
+                                    class="relative inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                                    :class="{ 'opacity-50 cursor-not-allowed': currentPage === pageCount }"
+                                    :disabled="currentPage === pageCount" @click="currentPage++">{{ $t('pagination.next') }}
+                                    &raquo;</button>
+                                <button v-if="surveySend" @click="submitAnswers"
+                                    class="relative inline-flex items-center rounded-md bg-orange-600 py-2 px-3 font-semibold text-white hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+                                    :class="{ 'opacity-50 cursor-not-allowed': hasError === 0 }" :disabled="hasError === 0">
+                                    <PaperAirplaneIcon class="h-5 w-5 mr-1" aria-hidden="true" /> {{ $t('survey.send') }}
+                                </button>
+                            </nav>
+                        </div>
                     </div>
                 </div>
-            </form>
+                <!-- mostrar mensaje error/success -->
+                <div class="p-4 text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert"
+                    v-if="hasError === 0">
+                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                        </circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>
+                    <span class="font-medium">{{ $t('survey.completed') }}</span> {{ $t('survey.success', { seconds: '5' })
+                    }}
+                </div>
+                <div class="p-4 text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert"
+                    v-if="hasError === 1">
+                    <span class="font-medium">{{ $t('survey.error') }}</span> {{ $t('survey.failed') }}
+                </div>
+            </div>
             <!-- mapa navegación -->
             <div class="px-4 py-5 text-base bg-orange-400 font-semibold leading-6 text-gray-900">{{ $t('survey.map') }}
             </div>
