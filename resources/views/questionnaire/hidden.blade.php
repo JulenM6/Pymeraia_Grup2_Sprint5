328c28e68b430c273b1348629c6cc87ad0446c0c
Julen Martinez <julenmartinez@iesmontsia.org>
2023-03-27 15:23:38 +0200

    .

--- resources/views/questionnaire/hidden.blade.php
+++ resources/views/questionnaire/hidden.blade.php
@@ -2,40 +2,40 @@
 
 @section('content')
 
-    <div class="overflow-x-auto max-w-4xl mx-auto  sm:rounded-lg m-5">
-        <div class="flex items-center justify-end">
+    <div class="m-5 flex items-center pt-4 justify-between ">
+        <div >
             <x-button-link href="{{route('questionnaire.index')}}">
                 {{__('questionnaire.active')}}
             </x-button-link>
         </div>
     </div>
 
-<div class="overflow-x-auto max-w-4xl mx-auto shadow-md sm:rounded-lg m-5">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
+    <div class="overflow-x-auto shadow-md sm:rounded-lg m-5">
+        <table class="table-auto w-full shadow-lg rounded-lg">
+        <thead >
+            <tr class="bg-orange-400 text-white">
+                <th scope="col" class="px-6 py-3 uppercase">
                     {{__('questionnaire.name')}}
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 uppercase">
                     {{__('questionnaire.autor')}}
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 uppercase">
                     {{__('questionnaire.date')}}
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 uppercase">
                     {{__('questionnaire.date.inactivate')}}
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3 uppercase">
                     {{__('actions')}}
                 </th>
             </tr>
         </thead>
         <tbody>
             @foreach ($questionnaires as $questionnaire)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr class="bg-orange-50 hover:bg-orange-100 text-center">
 
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <th scope="row" class="px-6 py-3 uppercase">
                     {{ $questionnaire->name }}
                 </th>
                 <td class="px-6 py-4">
@@ -48,12 +48,14 @@
                     {{ date('d-m-Y', strtotime($questionnaire->hidden)) }}
                 </td>
                 <td class="flex items-center px-6 py-4 space-x-3">
-                    <x-button-edit href="{{route('questionnaire.edit', $questionnaire)}}">
-                        <i class="fa-regular fa-pen-to-square"></i>
-                    </x-button-edit>
-                    <x-button-activate href="{{route('questionnaire.activate', $questionnaire)}}">
-                        <i class="fa-solid fa-arrow-up"></i>
-                    </x-button-activate>
+                <a class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-2 ml-2 rounded"
+                    href="{{route('questionnaire.edit', $questionnaire)}}">
+                    <i class="fa-regular fa-pen-to-square "></i>
+                </a>
+                <a class="bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-2 ml-2 rounded "
+                    href="{{route('questionnaire.activate', $questionnaire)}}">
+                    <i class="fa-solid fa-arrow-up"></i>
+                </a>
                 </td>
             </tr>
             @endforeach
