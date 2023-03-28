42ad607ea9d51bc5a0c2ebe94072541c40ade923
janalmudeve <joanpasqualalmudeve@iesmontsia.org>
2023-03-28 15:05:39 +0200

    API externa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <p class="p-5">Dashboard</p>
+    <h3>Consell del dia(Petició a API externa):</h3>
+    <div id="api-container" class="p-5"></div>
+
+    <script>
+        fetch('https://jsonplaceholder.typicode.com/comments/1')
+            .then((response) => response.json())
+            .then((json) => {
+                const apiContainer = document.getElementById('api-container');
+                apiContainer.innerHTML = `<p>${json.body}</p>`;
+            });
+    </script>
 @endsection
